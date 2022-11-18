bf525d3e81d7157e78d9145f8a89dcc2235bee85
Alina <alishad29@mail.ru>
2022-11-19 00:26:36 +0300

    max_min

---
+++ max_min.cs
@@ -0,0 +1,12 @@
+﻿int a = 5;
+int b = 7;
+int max = a;
+int min = a;
+
+//if (a > a) max = a;
+//if (a < a) min = a;
+if (b > a) max = b;
+if (b < a) min = b;
+
+Console.WriteLine ($"max = {max}");
+Console.WriteLine ($"min = {min}");
